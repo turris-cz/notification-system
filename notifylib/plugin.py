1414477f9a20ef065ebb8dfc7800f83ff268a344
Martin Matejek <martin.matejek@nic.cz>
2018-11-15 19:18:54 +0100

    Simple YAML schema validation for plugins

--- notifylib/plugin.py
+++ notifylib/plugin.py
@@ -5,6 +5,13 @@ from .logger import logger
 
 
 class Plugin:
+    SCHEMA = {
+        'actions': ['name', 'title', 'command'],
+        'templates': ['type', 'supported_media', 'src'],
+    }
+    # 'notifications' section is intentionally omitted for now
+    # until mandatory and optional attributes check for 'notifications' is implemented
+
     def __init__(self, name, actions, templates, notifications):
         self.name = name
         self.actions = {}
@@ -34,10 +41,38 @@ class Plugin:
             logger.warning("Failed to deserialize yaml file '%s'", filepath)
             return None
 
+        if not cls.valid_schema(data):
+            logger.warning("Failed to load plugin from file '%s' due to invalid YAML schema", f)
+            return None
+
+        # TODO: filter out extra unnecessary data
+        # i.e. anything not needed for plugin that is present in yaml file
+
         # Get only file name without suffix
         filename = pathlib.Path(filepath).stem
 
         return cls(filename, **data)
+
+    @classmethod
+    def valid_schema(cls, data):
+        """Simple validation of plugin structure similar to YAML schema validation"""
+        for key, values in cls.SCHEMA.items():
+            if key not in data.keys():
+                logger.warning("Mandatory section is missing: '%s'", key)
+                return False
+
+            data_vals = data[key]
+            schema_val_set = set(values)
+
+            for val in data_vals:
+                # set comparisons between schema.values and data_vals.keys()
+                diff = schema_val_set - set(val.keys())
+
+                if diff:
+                    logger.warning("Mandatory attributes are missing: %s", diff)
+                    return False
+
+        return True
 
     def get_actions(self):
         return self.actions
