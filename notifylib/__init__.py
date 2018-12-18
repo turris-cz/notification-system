61729839f4fa3198cd1062765e9f696615279462
Martin Matejek <martin.matejek@nic.cz>
2018-12-18 17:28:58 +0100

    Set __all__ imports for module

--- notifylib/__init__.py
+++ notifylib/__init__.py
@@ -1,1 +1,13 @@
+__all__ = [
+    'api',
+    'exceptions',
+    'notification',
+    'notificationskeleton',
+    'notificationstorage',
+    'plugin',
+    'pluginstorage',
+    'sorting',
+    'supervisor',
+]
+
 __version__ = '0.1'
