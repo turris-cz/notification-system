f67261ca8b64e1937e762c22255f12c2f2954b7b
Martin Matějek <martin.matejek@nic.cz>
2019-02-19 19:55:57 +0100

    PEP 8 compliant blank lines

--- notifylib/exceptions.py
+++ notifylib/exceptions.py
@@ -1,35 +1,46 @@
 class NotifylibError(Exception):
     pass
 
+
 class NotificationTemplatingError(NotifylibError):
     pass
+
 
 class CreateNotificationError(NotifylibError):
     pass
 
+
 class NoSuchActionError(NotifylibError):
     pass
+
 
 class MediaTypeNotAvailableError(NotifylibError):
     pass
 
+
 class NoSuchTemplateError(NotifylibError):
     pass
+
 
 class NoSuchNotificationError(NotifylibError):
     pass
 
+
 class NoSuchNotificationSkeletonError(NotifylibError):
     pass
+
 
 class NotificationNotDismissibleError(NotifylibError):
     pass
 
+
 class NotificationStorageError(NotifylibError):
     pass
 
+
 class VersionMismatchError(NotifylibError):
     pass
+
 
 class InvalidOptionsError(NotifylibError):
     pass
