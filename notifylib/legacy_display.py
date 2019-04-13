2487fa2a9b563168287ad411dbc343d084cfe9b8
Martin Matějek <martin.matejek@nic.cz>
2019-04-13 22:25:46 +0200

    user-notify-display: Allow dismiss of multiple ids at once

--- notifylib/legacy_display.py
+++ notifylib/legacy_display.py
@@ -7,14 +7,15 @@ from . import Api
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("ids", help="List of ids to mark as diplayed")
+    parser.add_argument("ids", help="List of ids to mark as diplayed", nargs="*")
 
     args = parser.parse_args()
 
     api = Api()
 
-    nid = args.ids.split('-')[0]
-    api.call_action(nid, 'dismiss')
+    for instance in args.ids:
+        nid = instance.split('-')[0]
+        api.call_action(nid, 'dismiss')
 
 
 if __name__ == '__main__':
